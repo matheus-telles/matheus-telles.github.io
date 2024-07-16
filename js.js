a71077975de51801b6947c3904c61f71b4471201
matheus-telles <matheussltmadureira@gmail.com>
2024-07-16 15:33:07 -0300

    dark-mode

--- js.js
+++ js.js
@@ -1,5 +1,6 @@
 const hamburguerButton = document.querySelector(".hamburguer");
 const menuElement = document.querySelector(".menu");
+const toggleDarkMode = document.querySelector(".toggleDarkMode");
 
 hamburguerButton.addEventListener("click", () => {
   const isOpen = hamburguerButton.classList.contains("open");
@@ -8,4 +9,14 @@ hamburguerButton.addEventListener("click", () => {
   menuElement.classList.toggle("open");
 
   hamburguerButton.setAttribute("aria-expanded", !isOpen);
+});
+
+toggleDarkMode.addEventListener("click", () => {
+  const body = document.body;
+  body.classList.toggle("light-mode");
+  if (body.classList == "light-mode") {
+    toggleDarkMode.innerText = "Dark";
+  } else {
+    toggleDarkMode.textContent = "Light";
+  }
 });
